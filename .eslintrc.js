7df1b3b779ee9faec6399ecc93b17a9270c39563
kingdom386 <707896515@qq.com>
2017-09-01 09:42:03 +0800

    VueMeizi
    vue福利版本

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    'generator-star-spacing': 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'semi':[2,'always'],
+    'indent':0,
+    'space-before-function-paren': 0
+  }
+}
---
+++ src/components/header/header.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="page-cover" v-show="menuShow" @click="isShow"></div>
+    <header class="header" :class="{'show': menuShow}" @click="isShow">
+      <header class="bar bar-nav">
+        <div class="pull-left">
+          <span class="iconfont icon-fenlei"></span>
+        </div>
+        <div class="title">{{headerTitle}}</div>
+        <div class="pull-right">
+          <span class="iconfont icon-sousuo_sousuo"></span>
+        </div>
+      </header>
+    </header>
+    <v-menu :show="menuShow"></v-menu>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import vMenu from '../menu/menu.vue';
+  import {mapState} from 'vuex';
+  export default
+  {
+    name: 'v-header',
+    components: {
+      vMenu
+    },
+    data() {
+      return {
+        show: false
+      };
+    },
+    computed: {
+      ...mapState([
+        'headerTitle', 'menuShow'// 创建映射,头部标题状态和 左侧菜单栏状态
+      ])
+    },
+    methods: {
+      isShow() {
+       this.$store.commit('UPDATE_MENUSHOW');
+      },
+      hideDetail() {
+        this.detailShow = false;
+      }
+    }
+  };
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "header.styl";
+</style>
+
